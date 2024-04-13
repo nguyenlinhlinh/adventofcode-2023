d4b856071ce0bc4c88e9593570c4e9d6215ff1ab
Linh Nguyen <nguyenlinhlinh@hotmail.com>
2024-04-13 16:04:39 +0200

    day 9 final

--- 9/solution2.py
+++ 9/solution2.py
@@ -1,31 +1,28 @@
-from collections import Counter
-from functools import cmp_to_key
-f= open("9/input-simple.txt", "r")
+f= open("9/input.txt", "r")
 total = 0
 for line in f:
     nbrs = [int(i) for i in line.strip().split()]
     nbrs.reverse()
-    differences = [nbrs]
-    print(differences)
     allZeros = False
+    lastRow = nbrs
+    lastNbrs = [nbrs[-1]] 
     while not allZeros:
         allZeros = True
         diff = []
-        lastRow = differences[-1]
         for i in range(len(lastRow) -1):
             a = lastRow[i]
             b = lastRow[i+1]
             diff.append(a - b)
             allZeros = allZeros and (a - b == 0) 
-        if(allZeros):
-            diff.append(0)
-        differences.append(diff)
-    for r in range(len(differences) -1 , -1 , -1):
-        a = differences[r][-1]
-        b = differences[r -1][-1]
-        c = b - a
-        differences[r -1].append(c)
-    total += differences[0][-1]
+        lastNbrs.append(diff[-1]) 
+        lastRow = diff
+
+    lastValue = 0
+    for r in range(len(lastNbrs) -1 , -1 , -1):
+        a = lastNbrs[r]
+        lastValue = a -lastValue
+        
+    total += lastValue
 
 print(total)
 
